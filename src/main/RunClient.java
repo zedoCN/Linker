f8d05774458a27fcc4162038d5d4456880619833
zedoCN <zedoCN@outlook.com>
2023-08-24 14:23:16 +0800

    update

---
+++ src/main/RunClient.java
@@ -0,0 +1,14 @@
+package main;
+
+import mode.LinkerClient;
+
+import java.util.Scanner;
+
+public class RunClient {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner=new Scanner(System.in);
+        LinkerClient client2 = new LinkerClient();
+        Thread.sleep(1000);
+        client2.joinGroup(scanner.nextLine(), 8888);
+    }
+}
