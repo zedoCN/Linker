df74fd5490222528c192fdc3366d111345092026
zedoCN <zedoCN@outlook.com>
2023-08-31 18:06:06 +0800

    update

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+chcp 65001
+java -Dfile.encoding=UTF-8 -jar Linker.jar
--- src/top/zedo/Main.java
+++ src/top/zedo/Main.java
@@ -2,11 +2,36 @@ package top.zedo;
 
 import com.sun.javafx.application.PlatformImpl;
 import com.sun.javafx.util.Logging;
+import top.zedo.net.LinkerServer;
 import top.zedo.ui.LinkerStage;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            runClient();
+        } else if (args.length == 1) {
+            switch (args[0]) {
+                case "-server":
+                    runServer();
+                case "-client":
+                    runClient();
+                default:
+                    System.out.println("-server 运行服务端\n-client 运行客户端\n");
+            }
+        }
 
+
+    }
+
+    /**
+     * 运行客户端
+     */
+    public static void runClient() {
         LinkerLogger.info("初始化图形系统");
 
         //屏蔽javafx歌姬初始化时的异常
@@ -21,6 +46,18 @@ public class Main {
             linkerStage.show();
 
         });
+    }
 
+    public static void runServer() {
+        Properties properties = new Properties();
+        try {
+            LinkerLogger.info("载入配置");
+            properties.load(new FileReader("./Linker.cfg", StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        LinkerServer linkerServer = new LinkerServer();
+        linkerServer.setServerPort(Integer.parseInt(properties.getProperty("linkerServerPort")));
+        linkerServer.start();
     }
 }
